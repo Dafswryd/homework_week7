a725d0af0c2daa3d5ee3f36819f27e20602ba8d9
Dafa Wira Yuda <dafwirayuda10@gmail.com>
2023-09-30 20:45:55 +0700

    homework week 7

---
+++ calculation.js
@@ -0,0 +1,23 @@
+// Menghitung Luas Persegi
+
+exports.mult = function (side, side) {
+    return side * side;
+};
+
+// Menghitung Keliling Persegi 
+
+exports.mult = function (sides, side) {
+    return sides * side;
+};
+
+// Menghitung Luas Persegi Panjang
+
+exports.mult = function (length, width) {
+    return length * width;
+};
+
+// Menghitung Keliling Persegi Panjang
+
+exports.mult = function (length, width) {
+    return 2 * (length + width);
+};
---
+++ soal1.js
@@ -0,0 +1,9 @@
+const calcs = require('./calculation');
+
+console.log("Luas Persegi : " + calcs.mult (5, 5));
+console.log("Keliling Persegi : " + calcs.mult (4, 5));
+
+console.log("Luas Persegi Panjang : " + calcs.mult (10, 8));
+console.log("Keliling Persegi Panjang : " + calcs.mult (6, 6));
+
+
